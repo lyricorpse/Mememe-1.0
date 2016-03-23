5c544f6062300b7bd61f0adf2bcdbd582d06ea99
Lyricorpse <lyricorpse@gmail.com>
2016-03-23 12:11:07 -0500

    Initial Commit

---
+++ Mememe 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mememe 1.0
+//
+//  Created by Feng Zhu on 3/23/16.
+//  Copyright © 2016 Feng Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
